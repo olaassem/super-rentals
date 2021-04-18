b569a53a9723c38b2fe136327473305a4c7e167a
Ola Assem <ola.assem@viget.com>
2021-04-18 08:57:53 +0200

    <Rental::Image> tests

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -3,24 +3,21 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | rental/image', function(hooks) {
+module('Integration | Component | rental/image', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<Rental::Image />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
+  test('it renders the given image', async function (assert) {
     await render(hbs`
-      <Rental::Image>
-        template block text
-      </Rental::Image>
+      <Rental::Image
+        src="/assets/images/teaching-tomster.png"
+        alt="Teaching Tomster"
+      />
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert
+      .dom('.image img')
+      .exists()
+      .hasAttribute('src', '/assets/images/teaching-tomster.png')
+      .hasAttribute('alt', 'Teaching Tomster');
   });
 });
